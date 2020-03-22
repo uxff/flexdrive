bae3ffa88af6d76691c0366031d1cf684d4e48fe
admin <uxff@qq.com>
2020-03-22 23:07:24 +0800

    add tool govscode

---
+++ govscode.sh
@@ -0,0 +1,24 @@
+#export GOPROXY=https://proxy.golang.org
+#go get -u -v github.com/bytbox/golint 
+#go get -u -v github.com/golang/tools 
+go get -u -v github.com/lukehoban/go-outline 
+go get -u -v github.com/newhook/go-symbols 
+go get -u -v github.com/josharian/impl 
+go get -u -v github.com/sqs/goreturns 
+go get -u -v github.com/cweill/gotests
+#go get -u golang.org/x/tools/...
+#go get -u -v github.com/nsf/gocode
+go get -u -v github.com/mdempsky/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v github.com/golang/lint/golint
+go get -u -v github.com/lukehoban/go-find-references
+#go get -u -v sourcegraph.com/sqs/goreturns
+go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v github.com/tpng/gopkgs
+go get -u -v github.com/newhook/go-symbols
+go get -u -v golang.org/x/tools/cmd/guru
+
+#additional
+#go get -v -u github.com/peterh/liner
+#go get -v -u github.com/derekparker/delve/cmd/dlv
+
