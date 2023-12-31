e33bf14399f9241a3a94c12522da045e2ad535f7
uxff <uxff@qq.com>
2023-12-31 23:01:59 +0800

    add cors

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -195,7 +195,7 @@ func ServeApi(addr string) error {
 	router := gin.New()
 	router.Use(func(c *gin.Context) {
 		// for cors, todo: should be closed in production environment
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173/") //*
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
