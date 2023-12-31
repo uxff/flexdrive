97bece4368dc12a521d54fd39e88e475ebdab832
uxff <uxff@qq.com>
2023-12-31 10:23:34 +0800

    update log

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -67,7 +67,7 @@ func main() {
 	lcf.Level.SetLevel(zapcore.Level(logLevel))
 	lcf.Development = true
 	lcf.DisableStacktrace = true
-	logger, err := lcf.Build(zap.AddCallerSkip(1))
+	logger, err := lcf.Build(zap.AddCallerSkip(1), zap.AddCaller())
 	if err != nil {
 		slog.Fatalln("new log err:", err.Error())
 	}
