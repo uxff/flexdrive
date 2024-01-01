35a5872dc33ce2745c78a24ca490a9a226421406
uxff <uxff@qq.com>
2024-01-01 09:19:53 +0800

    update cors

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -195,7 +195,7 @@ func ServeApi(addr string) error {
 	router := gin.New()
 	router.Use(func(c *gin.Context) {
 		// for cors, todo: should be re-configured in production environment
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173, http://192.168.164.129:10010") //*
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") //*
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Cookie")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
