2adf020dae7608ef7fb1182a43287c2c3af2a475
uxff <uxff@qq.com>
2024-01-01 08:47:32 +0800

    update cors

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -197,7 +197,7 @@ func ServeApi(addr string) error {
 		// for cors, todo: should be closed in production environment
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") //*
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Cookie")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
 		if c.Request.Method == "OPTIONS" {
