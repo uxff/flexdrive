4550d1405d2ec672b9b249449d209e89c715defa
uxff <uxff@hotmail.com>
2025-03-26 09:38:21 +0800

    optimize table

--- deploy/disk.sql
+++ deploy/disk.sql
@@ -1,6 +1,6 @@
 
 
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '会员id',
   `name` varchar(32) NOT NULL DEFAULT '' COMMENT '会员姓名',
   `email` varchar(32) NOT NULL COMMENT '邮箱',
@@ -20,7 +20,7 @@ CREATE TABLE `user` (
   UNIQUE KEY `IDX_email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `user_level` (
+CREATE TABLE IF NOT EXISTS `user_level` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '会员级别id',
   `name` varchar(32) NOT NULL COMMENT '会员级别名称',
   `quotaSpace` bigint(20) NOT NULL DEFAULT '0' COMMENT '会员级别的会员空间 单位KB',
@@ -34,7 +34,7 @@ CREATE TABLE `user_level` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `manager` (
+CREATE TABLE IF NOT EXISTS `manager` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '管理员id',
   `name` varchar(32) NOT NULL DEFAULT '' COMMENT '管理员名称',
   `phone` varchar(12) NOT NULL DEFAULT '' COMMENT '管理员手机号',
@@ -51,7 +51,7 @@ CREATE TABLE `manager` (
   UNIQUE KEY `IDX_email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `role` (
+CREATE TABLE IF NOT EXISTS `role` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '角色id',
   `name` varchar(32) NOT NULL DEFAULT '' COMMENT '角色名称',
   `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态 1=正常启用 99=删除',
@@ -61,7 +61,7 @@ CREATE TABLE `role` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `oper_log` (
+CREATE TABLE IF NOT EXISTS `oper_log` (
   `id` int(11) NOT NULL COMMENT '操作记录id',
   `managerId` int(11) NOT NULL COMMENT '操作员id 对应员工id',
   `managerName` varchar(32) NOT NULL DEFAULT '' COMMENT '操作人名称',
@@ -73,7 +73,7 @@ CREATE TABLE `oper_log` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `order` (
+CREATE TABLE IF NOT EXISTS `order` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '订单id',
   `userId` int(11) NOT NULL COMMENT '会员id',
   `originLevelId` int(11) NOT NULL COMMENT '会员原等级id',
@@ -91,11 +91,12 @@ CREATE TABLE `order` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `node` (
+CREATE TABLE IF NOT EXISTS `node` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `nodeName` varchar(40) NOT NULL COMMENT '节点名',
   `nodeAddr` varchar(32) NOT NULL COMMENT '节点地址 集群中服务地址',
   `clusterId` varchar(40) NOT NULL DEFAULT '' COMMENT '集群id',
+  `follow` varchar(40) NOT NULL DEFAULT '' COMMENT 'node name I follow',
   `totalSpace` bigint(20) NOT NULL DEFAULT '0' COMMENT '全部空间 单位KB',
   `usedSpace` bigint(20) NOT NULL DEFAULT '0' COMMENT '使用的空间 单位KB',
   `unusedSpace` bigint(20) NOT NULL DEFAULT '0' COMMENT '未使用空间',
@@ -109,7 +110,7 @@ CREATE TABLE `node` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- todo 分库分表 by hash
-CREATE TABLE `file_index` (
+CREATE TABLE IF NOT EXISTS `file_index` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '文件索引id',
   `fileName` varchar(64) NOT NULL DEFAULT '' COMMENT '文件名 无用',
   `fileHash` varchar(40) NOT NULL DEFAULT '' COMMENT '文件内容哈希',
@@ -129,7 +130,7 @@ CREATE TABLE `file_index` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- todo 分库分表 by uid
-CREATE TABLE `user_file` (
+CREATE TABLE IF NOT EXISTS `user_file` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '文件id',
   `fileIndexId` int(10) NOT NULL DEFAULT '0' COMMENT '文件索引id',
   `userId` int(11) NOT NULL DEFAULT '0' COMMENT '会员id',
@@ -149,7 +150,7 @@ CREATE TABLE `user_file` (
   INDEX IDX_User_PathHash (`userId`, `pathHash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `share` (
+CREATE TABLE IF NOT EXISTS `share` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '文件id',
   `fileHash` varchar(40) NOT NULL DEFAULT '' COMMENT '文件内容哈希',
   `shareHash`  varchar(40) NOT NULL DEFAULT '' COMMENT 'share哈希 用于路由',
@@ -166,7 +167,7 @@ CREATE TABLE `share` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
-CREATE TABLE `offline_task` (
+CREATE TABLE IF NOT EXISTS `offline_task` (
   `id` int(11) NOT NULL COMMENT '任务id',
   `userId` int(11) NOT NULL COMMENT '会员id',
   `dataurl` varchar(256) NOT NULL DEFAULT '' COMMENT '资源地址',
